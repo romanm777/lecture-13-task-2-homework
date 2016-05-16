85ffee4e9d336d01abf4c638a3d81a0e414f8321
romanm777 <roman1918@gmail.com>
2016-05-16 20:35:36 +0300

    Task 2 was partially done

--- js/base.js
+++ js/base.js
@@ -7,6 +7,8 @@ document.body.onkeydown = function (e) {
 
   var KEYCODE_LEFT = 37;
   var KEYCODE_RIGHT = 39;
+  var KEYCODE_UP = 38;
+  var KEYCODE_DOWN = 40;
 
   if (e.keyCode == KEYCODE_LEFT) {
   	el.style.left = (parseInt(el.style.left) - 10) + 'px';
@@ -14,5 +16,10 @@ document.body.onkeydown = function (e) {
   else if (e.keyCode == KEYCODE_RIGHT) {
   	el.style.left = (parseInt(el.style.left) + 10) + 'px';
   }
-
+  else if (e.keyCode == KEYCODE_UP) {
+    el.style.top = (parseInt(el.style.top) - 10) + 'px';
+  }
+  else if (e.keyCode == KEYCODE_DOWN) {
+    el.style.top = (parseInt(el.style.top) + 10) + 'px';
+  }
 }
